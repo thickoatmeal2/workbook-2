5dafaf56a31e4dcf2de321207ee836cd8af7fc05
Jacob <whatzdylan@gmail.com>
2025-04-19 20:11:46 -0400

    making progress

---
+++ CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,53 @@
+package com.pluralsight;
+
+public class CellPhone {
+
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone() {
+        this.serialNumber =0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+        }
+
+    public int getSerialNumber()  {
+        return serialNumber;
+
+    }
+
+    public void setSerialNumber (int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel (String model) {
+        this.model = model;
+        System.out.println(model);
+    }
+
+    public String getCarrier () {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+}
---
+++ CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
